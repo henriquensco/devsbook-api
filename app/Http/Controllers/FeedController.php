2db853636b4cf8632134757006850d0169a6012d
Henrique <nascimentocostahenrique@gmail.com>
2021-10-03 12:30:27 -0300

    Novas alteracoes

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illumnate\Support\Facades\Auth;
 use App\Models\Post;
+use App\Models\PostLike;
+use App\Models\PostComment;
+use App\Models\UserRelation;
+use App\Models\User;
 use Image;
 
 class FeedController extends Controller
@@ -79,6 +83,83 @@ class FeedController extends Controller
         }
 
         return $array;
+    }
+
+    public function read(Request $request) {
+        // GET api/feed (page)
+        $array = ['error' => ''];
+
+        $page = intval($request->input('page'));
+        $perPage = 2;
+
+        // 1. Pegar a lista de usuários que EU sigo (incluindo eu mesmo)
+        $users = [];
+        $userList = UserRelation::where('user_from', $this->loggedUser['id'])->get();
+
+        foreach ($userList as $userItem) {
+            $users[] = $userItem['user_to'];
+        }
+
+        $users[] = $this->loggedUser['id'];
+
+        // 2. Pegar os posts da galera ORDENADO PELA DATA
+        $postList = Post::whereIn('id_user', $users)
+            ->orderBy('created_at', 'desc')
+            ->offset($page * $perPage)
+            ->limit($perPage)
+            >get();
+
+        $total = Post::whereIn('id_user', $users)->count();
+        $pageCount = ceil($total / $perPage);
+
+        // 3. Preencher as informações adicionais
+        $posts = $this->_postListToObject($postList, $this->loggedUser['id']);
+        
+        $array['posts'] = $posts;
+        $array['pageCount'] = $pageCount;
+        $array['currentpage'] = $page;
+
+        return $array;
+    }
+
+    private function _postListToObject($postList, $loggedId) {
+        foreach($postList as $postKey => $postItem) {
+            
+            //Verificar se o post é meu
+            if($postItem['id'] == $loggedId) {
+                $postList[$postKey]['mine'] = true;
+            } else {
+                $postList[$postKey]['mine'] = false;
+            }
+
+            // Preencher informações de usuário
+            $userInfo = User::find($postItem['id_user']);
+            $userInfo['avatar'] = url('media/avatars/'.$userInfo['avatar']);
+            $userInfo['cover'] = url('media/covers/'.$userInfo['cover']);
+            $postList[$postKey]['user'] = $userInfo;
+
+            // Preencher informações de Like
+            $likes = PostLike::where('id_post', $postItem['id'])->count();
+            $postList[$postKey]['likeCount'] = $likes;
+
+            $isLiked = PostLike::where('id_post', $postItem['id'])
+                ->where('id_user', $loggedId)
+                ->count();
+            
+            $postLike[$postKey]['liked'] = ($isLiked > 0) ? true : false;
+
+            // Preencher informações de Comments
+            $comments = PostComment::where('id_post', $postItem['id'])->get();
+            foreach ($comments as $commentKey => $comment) {
+                $user = User::find($comment['id_user']);
+                $user['avatar'] = url('media/avatars/'.$userInfo['avatar']);
+                $user['cover'] = url('media/covers/'.$userInfo['cover']);
+                $comments[$commentKey]['user'] = $user;
+            }
+            $postList[$postKey]['comments'] = $comments;
+        }
+
+        return $postList;
     }
 
 }
